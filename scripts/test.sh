41e5669369a50a91121b17e162b751656d44884b
hugsbord <hugsbord@student.42.fr>
2022-02-08 16:29:38 +0100

    Cleaning repo

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-	read -p "Provide logged username target : " TARGET
-	if [[ $(users | grep $TARGET) != "" ]]; then
-		sudo pkill -u $TARGET; sudo userdel -f $TARGET; sudo rm -rf /home/$TARGET
-	else
-		echo "Provided username is not logged in or doesn't exist."
-	fi
- 
